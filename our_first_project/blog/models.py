bc690eba9c4dbf459cc5679dfe4b5007da5f3a1b
Pythonfriendss <816@wp.pl>
2021-07-18 12:48:26 +0200

    Simplest barebones

--- our_first_project/blog/models.py
+++ our_first_project/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title_text = models.CharField(max_length=200)
+    article_text = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.article_text[:60]
+
+
+class Comment(models.Model):
+    comment = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.comment_text[:30]
--- our_first_project/blog/views.py
+++ our_first_project/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Article
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'blog/templates/index.html'
+    context_object_name = 'blog_list'
+
+    def get_queryset(self):
+        context_object_name = 'article_list'
+        return Article.objects.order_by('-pub_date')[:10]
